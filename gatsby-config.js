bc682134954a902bc7efe2422e117c18497b591f
aaron-Splashtop <aaron.chan@splashtop.com>
2023-01-12 13:19:40 -0800

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `gatsby-v4-tutorial`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
